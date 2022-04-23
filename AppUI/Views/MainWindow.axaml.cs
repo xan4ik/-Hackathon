1c85943a464cac541a56fc8ed0257168a0bd376a
Hikaru Seki <inanislab@gmail.com>
2022-04-23 14:02:04 +0300

    :tada: initial commit

---
+++ AppUI/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace AppUI.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
